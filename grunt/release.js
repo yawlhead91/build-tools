723556fdf4cbc74cab1a086f17aee9eb76c2eafd
Mark Kennedy <mark.kennedy@akqa.com>
2015-03-30 18:17:55 -0400

    Changing so that correct version is passed to bump in release command

--- grunt/release.js
+++ grunt/release.js
@@ -2,7 +2,7 @@ var build = require('./../src/build');
 var bump = require('./../src/bump');
 
 module.exports = function(config, args) {
-    var version = args[1];
+    var version = args[0];
     return bump(version).then(function () {
         return build(config);
     });
