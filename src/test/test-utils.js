8a6650f42fe88e20910e517397cc591cd8ef010f
Mark Kennedy <mark.kennedy@akqa.com>
2015-04-15 01:00:57 -0400

    Using better event creation + changing so that events are bubbled and cancealable by default like browser

--- src/test/test-utils.js
+++ src/test/test-utils.js
@@ -6,22 +6,16 @@ module.exports = {
     /**
      * Creates an event.
      * @param {string} name - The event name
-     * @param {object} options - Options to be passed to event
+     * @param {object} [options] - Options to be passed to event
      */
     createEvent: function (name, options) {
         var event;
-
         options = options || {};
-        options.bubbles = options.bubbles || false;
-        options.cancelable = options.cancelable|| false;
+        options.bubbles = options.bubbles || true;
+        options.cancelable = options.cancelable|| true;
 
-        if (typeof Event === 'function') {
-            event = new Event(name, options);
-        } else {
-            // must register click old-fashioned way so that running tests headlessly will work
-            event = document.createEvent('Event');
-            event.initEvent(name, options.bubbles, options.cancelable);
-        }
+        event = document.createEvent('CustomEvent');
+        event.initCustomEvent(name, options.bubbles, options.cancelable, null);
         return event;
     },
 
@@ -56,22 +50,6 @@ module.exports = {
             val = val.trim();
         }
         return val;
-    },
-
-    /**
-     * Creates and returns a promise that can be resolved or rejected.
-     * @returns {*}
-     */
-    createPromise: function () {
-        var resolve,
-            reject,
-            promise = new Promise(function (res, rej) {
-                resolve = res;
-                reject = rej;
-            });
-        promise.resolve = resolve;
-        promise.reject = reject;
-        return promise;
     }
 
 };
